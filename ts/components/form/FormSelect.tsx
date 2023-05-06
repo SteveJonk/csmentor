016973a004f0879493bf3daa9409c10cc454a371
Steve Jonk <stevejonk1993@gmail.com>
2023-05-06 10:10:33 +0200

    feat: possibility to clear select

--- ts/components/form/FormSelect.tsx
+++ ts/components/form/FormSelect.tsx
@@ -1,4 +1,6 @@
+import ClearIcon from '@mui/icons-material/Clear'
 import FormControl from '@mui/material/FormControl'
+import IconButton from '@mui/material/IconButton'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
@@ -16,6 +18,10 @@ export const FormSelect = ({ label, multiple, onChange, options, selectedValue }
     onChange(event.target.value)
   }
 
+  const handleClear = () => {
+    onChange('')
+  }
+
   return (
     <FormControl fullWidth size="small">
       <InputLabel>{label}</InputLabel>
@@ -26,6 +32,11 @@ export const FormSelect = ({ label, multiple, onChange, options, selectedValue }
         onChange={handleChange}
         size="small"
         variant="outlined"
+        endAdornment={
+          <IconButton sx={{ display: selectedValue ? '' : 'none' }} onClick={handleClear}>
+            <ClearIcon fontSize="small" />
+          </IconButton>
+        }
       >
         {options?.map((option) => (
           <MenuItem key={option} value={option}>
